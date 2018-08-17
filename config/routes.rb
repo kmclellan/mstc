2b01addb7e840a3e049c20f40c4b77572dde3e65
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-17 18:43:33 +0000

    Added basic pages and configured databases

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/help'
-  get 'pages/contact'
-  get 'pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'application#test'
+  get   'pages/home'
+  get   'pages/help'
+  get   'pages/contact'
+  get   'pages/about'
+  root  'application#test'
 end
