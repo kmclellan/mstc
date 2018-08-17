5664113196005e4bc2ab47d5612e118687271c1f
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-17 19:17:32 +0000

    Finished creating basic pages and configured testing gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root  'pages#home'
   get   'pages/home'
   get   'pages/help'
   get   'pages/contact'
   get   'pages/about'
-  root  'application#test'
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,24 +1,39 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "MSTC"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get contact" do
     get pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
