c79f5a6502f2cc9c70dc12e96c8d7c102f74ec4e
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-18 19:35:16 +0000

    Finished creating basic users controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates  :firstname,   presence: true, length: { maximum: 25  }
+  validates  :lastname,    presence: true, length: { maximum: 25  }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates  :email,      presence: true,  length: { maximum: 200 },
+             format: { with: VALID_EMAIL_REGEX },
+             uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_18_192653) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
