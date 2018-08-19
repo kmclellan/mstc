2492da5a63c8be6d746d39e6e8f93e869aeac12c
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-19 18:09:57 +0000

    Fixed admin edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:index, :edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -66,7 +66,8 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user) || current_user.admin?
+
     end
 
     def admin_user
