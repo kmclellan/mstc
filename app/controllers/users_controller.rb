1d2aa31cc804ecc3c7bc42269adf64a58569bc20
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-19 14:36:27 +0000

    users list basic functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
     @user = User.new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(firstname:  "William",
+             lastname: "Bland",
+             email: "admin@email.com",
+             password:              "12345678",
+             password_confirmation: "12345678")
+
+50.times do |n|
+  firstname  = Faker::Name.first_name
+  lastname  = Faker::Name.last_name
+  email = "basic.user.#{n+1}@email.com"
+  password = "12345678"
+  User.create!(firstname:  firstname,
+               lastname: lastname,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
