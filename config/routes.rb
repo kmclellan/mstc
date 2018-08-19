504af5fad76552fda3f38036a06c48e02d3ec236
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-19 23:53:06 +0000

    Add basic password reset functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get    'sessions/new'
   get    'users/new'
   root   'pages#home'
@@ -13,4 +15,5 @@ Rails.application.routes.draw do
   delete '/logout',    to: 'sessions#destroy'
   resources :users
   resources :user_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
