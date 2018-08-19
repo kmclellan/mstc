b97f3e97aa6654ba83f8b4b2146d28d5cadd97f2
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-19 21:02:34 +0000

    Fixed user settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:index, :edit, :update, :destroy]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user) || current_user.admin?
   end
 
   def create
