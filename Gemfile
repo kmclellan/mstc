dfb91291e75e6be7da48e55e364bda1b43d04708
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-21 14:19:46 +0000

    Added bootstrap themeing with bootswatch gem

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'jquery-rails',            '4.3.1'
 gem 'turbolinks',              '5.0.1'
 gem 'jbuilder',                '2.7.0'
 gem 'bootsnap', require: false
+gem 'bootswatch-rails'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_232605) do
+ActiveRecord::Schema.define(version: 2018_08_20_173151) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_08_19_232605) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
