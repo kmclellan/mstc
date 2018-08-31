a814a226fcf586cb546b8c3ecd2aef705fa555e6
William Thomas Bland <w.bland.16@aberdeen.ac.uk>
2018-08-31 14:26:06 +0000

    Removed user_type from users table

---
+++ db/migrate/20180831142135_remove_user_type_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveUserTypeFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :user_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_173151) do
+ActiveRecord::Schema.define(version: 2018_08_31_142135) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2018_08_20_173151) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
